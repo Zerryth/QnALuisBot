66f3a10e6267d5966a2663002f934022a76934e2
Ashley Ho (Teksystems) <v-asho@microsoft.com>
2018-08-07 16:07:47 -0700

    added LuisIntent StudyBiology which calls Biology QnA Service

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -32,8 +32,26 @@ namespace Microsoft.Bot.Sample.LuisBot
         static readonly string KBId = ConfigurationManager.AppSettings["KnowledgeBaseId"];
         static readonly string QnAEndpointKey = ConfigurationManager.AppSettings["QnAAuthKey"];
 
+        //WINONA'S KEYS
+        static readonly string BiologyQnAHostName = ConfigurationManager.AppSettings["BiologyQnAEndpointHostName"];
+        static readonly string BiologyKBId = ConfigurationManager.AppSettings["BiologyKBId"];
+        static readonly string BiologyEndpointKey = ConfigurationManager.AppSettings["BiologyQnAAuthKey"];
+
+
+
         // Instantiate the QnAMakerService class for each of your knowledge bases
-        public QnAMakerService BitLockerQnAService = new QnAMakerService(QnAHostName, KBId, QnAEndpointKey); 
+        public QnAMakerService BitLockerQnAService = new QnAMakerService(QnAHostName, KBId, QnAEndpointKey);
+
+        // WINONA'S KBs
+        public QnAMakerService biologyQnAService = new QnAMakerService
+        (BiologyQnAHostName, BiologyKBId, BiologyEndpointKey);
+        //("https://qnamakerwin.azurewebsites.net/qnamaker", "3b2e0ac6-11bd-48c8-bdfb-759462d8502a", "45217269-2304-4b74-9155-e98795a87b12");
+
+        public QnAMakerService sociologyQnAService = new QnAMakerService
+        ("https://qnamakerwin.azurewebsites.net/qnamaker", "43c68327-b8c1-4398-882b-11b09cfdae7c", "f038dceb-289d-445f-87dc-81cefe57eed6");
+        public QnAMakerService geologyQnAService = new QnAMakerService
+        ("https://qnamakerwin.azurewebsites.net/qnamaker", "4f080760-9f77-4f2e-9b8b-1945f6c16f1a", "f038dceb-289d-445f-87dc-81cefe57eed6");
+
 
         [LuisIntent("None")]
         public async Task NoneIntent(IDialogContext context, LuisResult result)
@@ -66,6 +84,13 @@ namespace Microsoft.Bot.Sample.LuisBot
         {
             await this.ShowLuisResult(context, result);
             await context.PostAsync(BitLockerQnAService.GetAnswer(result.Query));
+        }
+
+        [LuisIntent("StudyBiology")]
+        public async Task StudyBiologyIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("In study biology intent!");
+            await context.PostAsync(biologyQnAService.GetAnswer(result.Query));
         }
 
         private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
