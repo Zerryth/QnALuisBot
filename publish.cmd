ec76dd92f16ace0525e9bbd01eebb4a3be271416
Ashley Ho (Teksystems) <v-asho@microsoft.com>
2018-08-07 12:17:34 -0700

    initial commit. LUIS + QnA Maker skeleton

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=ashleyhoqnaluistutorial-Web-Deploy.pubxml -p:Password=g3JleHtDQfW38uaLcjvzmRqo5RJzcawLoqpmpg6Zp6dKXThKNsl6s3hHTrQ6
+
